b518764d3865dc55a9fd760b54f9841b749e1c73
kentana20 <tanakak@eac.ne.jp>
2017-02-11 15:34:10 +0900

    add vue files

--- vue-giphy/src/App.vue
+++ vue-giphy/src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <vue-header></vue-header>
+    <time-line></time-line>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import VueHeader from './components/Header'
+import TimeLine from './components/TimeLine'
 
 export default {
-  name: 'app',
   components: {
-    Hello
+    VueHeader,
+    TimeLine
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
